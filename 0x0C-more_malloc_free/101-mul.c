6a19231403f96c8b622e654b00ab891316553401
NekaMoks <nekaogbue@gmail.com>
2023-01-14 00:03:45 +0100

    Memory Allocation

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _atoi_digit - convert a char to integer.
+ * @x: character to convert
+ * Return: integer
+ **/
+
+int _atoi_digit(char x)
+{
+	unsigned int res;
+
+	if (x <= '9' && x >= '0')
+		res = x - '0';
+	return (res);
+}
+
+/**
+ * _isNumber - Define if a string is a number
+ * @argv: pointer to string
+ * Return: success (0)
+ **/
+int _atoi_digit(char x)
+{
+	unsigned int res;
+
+	if (x <= '9' && x >= '0')
+		res = x - '0';
+	return (res);
+}
+
+/**
+ * _isNumber - Define if a string is a number
+ * @argv: pointer to string
+ * Return: success(0)
+ **/
+int _isNumber(char *
